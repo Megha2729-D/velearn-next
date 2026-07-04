cd609f620c2d88379c140517ec4acd443f7a9a17
Megha2729-D <meghu27022002@gmail.com>
2026-07-04 16:49:42 +0530

    Updated project files

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://velearn.in/velearn-crm/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
